8c98fdd080400a501ec4dc10acd60deb3064baf2
AndrewSeliverstov <aardvarkd101@gmail.com>
2021-08-21 19:51:01 +0300

    fix: add check

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+  if ((arguments.length === 0)||(array.length === 0 )) {
+    return 0;
+  } else {
+    let min = Math.min( ...array );
+    return min;
+  }
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if ((arguments.length === 0)||(array.length === 0 )) {
+    return 0;
+  } else {
+    let max = Math.max( ...array );
+    return max;
+  }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if ((arguments.length === 0)||(array.length === 0 )) {
+    return 0;
+  } else {
+      let average = 0;
+      let sum = 0;
+
+      for (let i = 0; i < array.length; i++) {
+          sum += (array[i]);
+          average = sum / array.length;
+      }
+      return average;
+    } 
+};
